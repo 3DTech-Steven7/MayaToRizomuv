05a2014ad6f963f1220d344496fdefc4345dc3b3
qsc <625002974@qq.com>
2021-04-10 20:26:45 +0800

    fix

--- install_modules.cmd
+++ install_modules.cmd
@@ -3,7 +3,9 @@ chcp 65001
 set mod_name=MayaToRizomUV.mod
 set loacl_mod=%cd%\modules
 set user_mod=%USERPROFILE%\Documents\maya\modules
-
+if exist %loacl_mod% == 0(
+   md %loacl_mod%
+)
 echo + MayaToRizomUV 1.0.1 %cd% > %loacl_mod%\%mod_name%
 if exist %user_mod% == 0(
    md %user_mod%
